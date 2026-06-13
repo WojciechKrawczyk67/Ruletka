ef3241cab90d3ac1caa315aeaa8349c03db65a4f
33wojtek33 <ebekrawczyk@student.agh.edu.pl>
2026-06-13 14:33:14 +0200

    feat(roulette): add Wheel header and cpp skeleton

---
+++ untitled/Wheel.cpp
@@ -0,0 +1,10 @@
+//
+// Created by wojte on 13.06.2026.
+//
+
+#include "Wheel.h"
+
+Wheel::Wheel() {
+int Wheel::spin() {
+    return 0;
+}
---
+++ untitled/Wheel.h
@@ -0,0 +1,16 @@
+//
+// Created by wojte on 13.06.2026.
+//
+
+#ifndef UNTITLED_WHEEL_H
+#define UNTITLED_WHEEL_H
+
+
+#pragma once
+
+class Wheel {
+public:
+    Wheel();
+
+    int spin();
+};
