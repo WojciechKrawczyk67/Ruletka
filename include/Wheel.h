e06e5ac2390cbf86b8500cb20e84dafa697d9e9e
Wojciech Krawczyk <430005@student.agh.edu.pl>
2026-06-15 18:27:58 +0200

    Końcowe poprawki i test CZY DZIAŁA zakończony sukcesem

---
+++ include/Wheel.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Wheel {
+public:
+    Wheel();
+    int spin();
+};
---
+++ src/Wallet.cpp
@@ -0,0 +1,21 @@
+#include "Wallet.h"
+
+Wallet::Wallet(double initial_balance) : balance(initial_balance) {}
+
+void Wallet::addFunds(double amount) {
+    if (amount > 0) {
+        balance += amount;
+    }
+}
+
+bool Wallet::deductFunds(double amount) {
+    if (amount <= 0 || amount > balance) {
+        return false;
+    }
+    balance -= amount;
+    return true;
+}
+
+double Wallet::getBalance() const {
+    return balance;
+}
